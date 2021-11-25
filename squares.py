1a892f41f3629a8c9535bfd457cbc7a0c21c50f5
Thomas <thomasclarke28@hotmail.co.uk>
2021-11-25 11:51:30 +0000

    Argparse command line implemented

--- squares.py
+++ squares.py
@@ -1,3 +1,4 @@
+from argparse import ArgumentParser
 def average_of_squares(list_of_numbers, list_of_weights=None):
     """ Return the weighted average of a list of values.
     
@@ -46,12 +47,16 @@ def convert_numbers(list_of_strings):
 
 
 if __name__ == "__main__":
-    numbers_strings = ["1","2","4"]
+    parser = ArgumentParser(description="Generate average") #comes in the help bit
+    parser.add_argument('numbers', nargs="+",type=float,help="The numbers to make an average from")
+    arguments= parser.parse_args()
+
+    #numbers_strings = ["1","2","4"]
     weight_strings = ["1","1","1"]        
     
-    numbers = convert_numbers(numbers_strings)
+#    numbers = convert_numbers(numbers_strings)
     weights = convert_numbers(weight_strings)
     
-    result = average_of_squares(numbers, weights)
+    result = average_of_squares(arguments.numbers, weights)
     
     print(result)
